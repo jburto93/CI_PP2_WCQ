e960e0f36270f7949ed77ab7384f5c39982e8f50
jburto93 <jonathan-burton@live.co.uk>
2022-05-29 12:28:43 +0000

    add initial code in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progress-text = document.querySelector('#progress-text');
+const score-text = document.querySelector('#score');
+const progress-bar-full = document.querySelector('#progress-bar-full');
