366a39bd8b1799a0d3ed6c5fb0e6af21d77cc549
jburto93 <jonathan-burton@live.co.uk>
2022-06-05 19:22:28 +0000

    add initial html to leaderboard.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,3 +135,22 @@ username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value
 })
 
+saveHighScore = e => {
+    e.preventDefault()
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+
+    highScores.push(score)
+
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    highScores.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('/')
+}
